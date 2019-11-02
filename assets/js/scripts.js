d12e98925d3d93b624f2fbf6a8713897a5e1ff25
cambragol <griffon_academy@yahoo.ca>
2019-11-02 19:57:56 +0900

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,23 +25,6 @@ $(document).ready(function() {
 });
 
 
-$(function(){
-  $('<img>').attr('src',function(){
-      var imgUrl = $('div.featured-image').css('background-image');
-      if (!imgUrl) {
-        return;
-      }
-      var urlre = /url\([\"\']?(.*?)[\"\']?\)/;
-      imgUrl = imgUrl.match(urlre);
-      if($.isArray(imgUrl)) {
-        imgUrl = imgUrl[1]; // Captured subexpression.
-      }
-      return imgUrl;
-  }).load(function(){
-    $('img.loading').fadeOut(500);
-    $('div.overlay').fadeTo("slow", 0.6);
-  });
-});
 
 $(function(){
     $('.post-list li').each(function(i){
